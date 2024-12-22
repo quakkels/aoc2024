789d44f1fa99b7957dc1f7cb7efe18045ec08099
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-12-22 15:54:04 -0600

    day6part1: I thought this would work, but it didn't

--- six/main.go
+++ six/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -36,38 +37,130 @@ func main() {
 	fmt.Println("part1>", result1)
 }
 
+type PositionsHashSet struct {
+	hashSet map[string]struct{}
+}
+
+func newPositionsHashSet() PositionsHashSet {
+	return PositionsHashSet{
+		hashSet: make(map[string]struct{}),
+	}
+}
+
+func (hs *PositionsHashSet) Add(value []int) {
+	var key string = strconv.Itoa(value[0]) + strconv.Itoa(value[1])
+	hs.hashSet[key] = struct{}{}
+}
+
+func (hs *PositionsHashSet) Contains(value []int) bool {
+	var key string = strconv.Itoa(value[0]) + strconv.Itoa(value[1])
+	_, ok := hs.hashSet[key]
+	return ok
+}
+
+func (hs *PositionsHashSet) Size() int {
+	return len(hs.hashSet)
+}
+
 type part1 struct {
 	input []string
-	turns map[byte][]int
+	directions []byte
+	currentDirection byte
+	currentPosition []int
+	vectors map[byte][]int
+	visitedPositions PositionsHashSet
 }
 
 func newPart1(input []string) part1{
 	p1 := part1{}
-	p1.turns = make(map[byte][]int)
-	p1.turns['^'] = []int{0, 1} // turn east
-	p1.turns['>'] = []int{1, 0} // turn south
-	p1.turns['v'] = []int{0, -1} // turn west
-	p1.turns['<'] = []int{-1, 0} // turn north
+
+	p1.directions = []byte {
+		'>',
+		'v',
+		'<',
+		'^',
+	}
+
+	p1.vectors = make(map[byte][]int)
+	p1.vectors['>'] = []int{0, 1} // east
+	p1.vectors['v'] = []int{1, 0} // south
+	p1.vectors['<'] = []int{0, -1} // west
+	p1.vectors['^'] = []int{-1, 0} // north
 
 	p1.input = input
+
+	p1.currentPosition, p1.currentDirection = p1.findStart()
+
+	p1.visitedPositions = newPositionsHashSet()
+	p1.visitedPositions.Add(p1.currentPosition)
 
 	return p1
 }
 
 func (p1 *part1) part1() int {
-	result := 0
-	return result
+	for {
+		// move one cell
+		nexti := p1.currentPosition[0] + p1.vectors[p1.currentDirection][0]
+		nextj := p1.currentPosition[1] + p1.vectors[p1.currentDirection][1]
+
+		isObstacle, isOffMap := p1.isObstacle(nexti, nextj)
+		if isObstacle {
+			// the next cell is a wall
+			// so turn and try again
+			p1.currentDirection = p1.getRightTurn()
+			if isOffMap {
+				break // off the edge of the world
+			}
+			continue
+		}
+
+		// we move
+
+		p1.currentPosition[0] = nexti
+		p1.currentPosition[1] = nextj
+		p1.visitedPositions.Add(p1.currentPosition)
+	}
+
+	return p1.visitedPositions.Size()
 }
 
-func (p1 *part1) findGuard() []int {
+func (p1 *part1) findStart() ([]int, byte) {
 	for i := range p1.input {
 		for j := range p1.input[i] {
-			_, exists := p1.turns[p1.input[i][j]]
+			_, exists := p1.vectors[p1.input[i][j]]
 			if exists {
-				return []int {i, j}
+				return []int {i, j}, p1.input[i][j]
 			}
 		}
 	}
 
-	return []int{-1,-1}
+	return []int{-1,-1}, 0 
 }
+
+func (p1 *part1) isInbounds() bool {
+	return len(p1.input) > p1.currentPosition[0] && len(p1.input[0]) > p1.currentPosition[1]
+}
+
+func (p1 *part1) getRightTurn() byte {
+	// oof, what am I doing here?
+	nextIndex := 0
+	for i := range p1.directions {
+		if p1.directions[i] == p1.currentDirection {
+			nextIndex = i + 1
+		}
+	}
+
+	if nextIndex >= len(p1.directions) {
+		nextIndex = 0
+	}
+	return p1.directions[nextIndex]
+}
+
+func (p1 *part1) isObstacle(i, j int) (isObstacle, isOffMap bool) {
+	if len(p1.input) <= i || len(p1.input[0]) <= j {
+		return true, true
+	}
+
+	return p1.input[i][j] == '#', false
+}
+
--- six/main_test.go
+++ six/main_test.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	//"log"
+	"log"
 	"testing"
 )
+
+func TestRightTurn(t *testing.T) {
+	input := []string {
+		">",
+	}
+
+	p1 := newPart1(input)
+
+	if p1.currentDirection != '>' {
+		t.Fatal("Expected result: >. Actual:", p1.currentDirection)
+	}
+
+	p1.currentDirection = p1.getRightTurn()
+	if p1.currentDirection != 'v' {
+		t.Fatal("Expected result: v. Actual:", p1.currentDirection)
+	}
+
+	p1.currentDirection = p1.getRightTurn()
+	if p1.currentDirection != '<' {
+		t.Fatal("Expected result: <. Actual:", p1.currentDirection)
+	}
+
+	p1.currentDirection = p1.getRightTurn()
+	if p1.currentDirection != '^' {
+		t.Fatal("Expected result: ^. Actual:", p1.currentDirection)
+	}
+}
 
 func TestFindGuard(t *testing.T) {
 	inputa := []string {
@@ -12,10 +39,14 @@ func TestFindGuard(t *testing.T) {
 	}
 
 	p1a := newPart1(inputa)
-	resulta := p1a.findGuard()
+	resulta, dira := p1a.findStart()
 
 	if resulta[0] != 1 || resulta[1] != 1 {
 		t.Fatal("Expected result: 1,1. Actual:", resulta)
+	}
+	
+	if dira != '>' {
+		t.Fatal("Expected result: >. Actual:", string(dira))
 	}
 
 	inputb := []string {
@@ -24,10 +55,14 @@ func TestFindGuard(t *testing.T) {
 	}
 
 	p1b := newPart1(inputb)
-	resultb := p1b.findGuard()
+	resultb, dirb := p1b.findStart()
 
 	if resultb[0] != 1 || resultb[1] != 1 {
 		t.Fatal("Expected result: 1,1. Actual:", resultb)
+	}
+
+	if dirb != 'v' {
+		t.Fatal("Expected result: v. Actual:", string(dirb))
 	}
 
 	inputc := []string {
@@ -36,10 +71,14 @@ func TestFindGuard(t *testing.T) {
 	}
 
 	p1c := newPart1(inputc)
-	resultc := p1c.findGuard()
+	resultc, dirc := p1c.findStart()
 
 	if resultc[0] != 1 || resultc[1] != 1 {
 		t.Fatal("Expected result: 1,1. Actual:", resultc)
+	}
+
+	if dirc != '<' {
+		t.Fatal("Expected result: <. Actual:", string(dirc))
 	}
 
 	inputd := []string {
@@ -48,10 +87,30 @@ func TestFindGuard(t *testing.T) {
 	}
 
 	p1d := newPart1(inputd)
-	resultd := p1d.findGuard()
+	resultd, dird := p1d.findStart()
 
 	if resultd[0] != 1 || resultd[1] != 1 {
 		t.Fatal("Expected result: 1,1. Actual:", resultd)
+	}
+
+	if dird != '^' {
+		t.Fatal("Expected result: ^. Actual:", string(dird))
+	}
+}
+
+func TestIsInbounds(t *testing.T) {
+	input := []string {
+		"..",
+		"..",
+	}
+
+	p1 := newPart1(input)
+	p1.currentPosition = []int{0,0}
+
+	isInboundsA := p1.isInbounds()
+
+	if !isInboundsA {
+		t.Fatal("Expected: true. Actual:", isInboundsA)
 	}
 }
 
@@ -68,6 +127,7 @@ func TestPart1(t *testing.T) {
 		"#.........",
 		"......#...",
 	}
+	log.Println(input)
 
 	p1 := newPart1(input)
 	result := p1.part1()
